5383dc76848b5d442d9b37294576931c899d1128
azotamiota <azotamiota@gmail.com>
2022-09-03 23:22:54 +0100

    add inputRef

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,27 @@ const socket = io.connect('https://millionare-chat.herokuapp.com/')
 function App() {
 
   const me = useRef(new Date().getTime())
-  console.log('it is me: ', me);
+  const inputRef = useRef(null);
+  const messageContainer = useRef([])
    const [message, setMessage] = useState('')
-   const messageContainer = useRef([])
    const [, forceUpdate] = useReducer(x => x + 1, 0);
-  //  const [messagesReceived, setMessagesReceived] = useState('')
-  //  const [allMessages, setAllMessages] = useState([])
+  
 
   const updateAllMessages = (e) => {
     e.preventDefault();
-    // messageContainer.current.push(message)
     socket.emit('send_message', [{id: me.current, message: message }])
     setMessage('')    
   }
   
   useEffect(()=>{
-    
+    inputRef.current.focus();
     socket.on('receive_message', (data) => {
-      console.log('incoming messages from backend: ', data[0])
       messageContainer.current.push(data[0])
-      console.log('current messagesREceived: ', messageContainer.current[0])
-      // setAllMessages([...allMessages, messageReceived])
       
-        forceUpdate()
+      forceUpdate()
       
       })
   },[socket])
-
-
 
 
   return (
@@ -42,7 +35,7 @@ function App() {
       <h1>Message:</h1>
       {messageContainer.current.map(message => <div className={message.id === me.current ? 'my-message' : 'foreign-message'} key={Math.random()}>{message.message}</div>)}
     <form className='chat-input' onSubmit={updateAllMessages}>
-      <input type='text' placeholder='Send your message...' onChange={(e) => {setMessage(e.target.value)}} value={message} required/>
+      <input ref={inputRef} type='text' placeholder='Send your message...' onChange={(e) => {setMessage(e.target.value)}} value={message} required/>
       <input type='submit' value='Send Message'/>
     </form>
     </div>
